8d8261a17329c95591cad85e88ec612ee8a80839
vladimirshkarin <vladimir.shkarin@gmail.com>
2024-07-14 21:36:43 +0300

    Refactoring.

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,11 @@
 #!usr/bin/env python3
 
-from brain_games.engine import engine
+from brain_games.engine import game_logic
 from brain_games.games.calc import get_exercise, task
 
 
 def main():
-    engine(get_exercise, task)
+    game_logic(get_exercise, task)
 
 
 if __name__ == "__main__":

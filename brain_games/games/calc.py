94f49ef5d9b28f7039898e4ae0b79e999420cca4
vladimirshkarin <vladimir.shkarin@gmail.com>
2024-07-14 21:28:37 +0300

    Calc.py refactoring.

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,14 +5,18 @@ import operator
 task = "What is the result of the expression?"
 
 
-def run_exercise():
+def get_action(type, *args):
     action = {
         "+": operator.add,
         "-": operator.sub,
         "*": operator.mul,
     }
-    type = random.choice(list(action))
+    return action[type](*args)
+
+
+def run_exercise():
+    type = random.choice(['+', '-', '*'])
     numbers = random.randint(1, 999), random.randint(1, 999)
-    exercise = type, numbers
-    correct = action[type](numbers)
+    exercise = f"{numbers[0]} {type} {numbers[1]}"
+    correct = get_action(type, numbers)
     return exercise, correct
